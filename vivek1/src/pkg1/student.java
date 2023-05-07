4e7edd6544e9fba0fba8f3818f82091979ce6bfb
hp <hp@192.168.0.102>
2023-05-07 12:07:46 +0530

    pushing the code first time

---
+++ vivek1/src/pkg1/student.java
@@ -0,0 +1,26 @@
+package pkg1;
+
+public class student 
+
+{   // boundary(body) of the class
+	
+	int deepak; // created a variable named deepak
+	public void abc ()  // method name abc
+	{ // boundary of the method
+		System.out.println("welcome to all of you");
+		
+	}
+	public static void main(String[] args) 
+	{  // boundary(body)of the main method
+		student xyz=new student(); // piller3
+		xyz.abc (); // calling the method 
+		xyz.deepak=122; //calling the variable
+		System.out.println(xyz.deepak);
+		
+		
+		
+	}
+	
+
+}
+
